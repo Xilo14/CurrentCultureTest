df2b5db0bdbd3ce6c562b88177a10cf440df13a1
Xilosof <pro100temi444@gmail.com>
2021-08-10 12:05:41 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CurrentCultureTest
+{
+    class Program
+    {
+        static ConcurrentDictionary<CultureInfo, int> counts1 = new();
+        static ConcurrentDictionary<int, int> threads1 = new();
+        static ConcurrentDictionary<CultureInfo, int> counts2 = new();
+        static ConcurrentDictionary<int, int> threads2 = new();
+        static void Main(string[] args)
+        {
+            var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            var tasks = cultures.Select(c => Work(c));
+
+            Task.WaitAll(tasks.ToArray());
+
+            Console.WriteLine($"Total amount of cultures: {cultures.Count()}");
+            Console.WriteLine($"Total amount pairs in 'counts1': {counts1.Count()}");
+            Console.WriteLine($"Total amount pairs in 'threads1': {threads1.Count()}");
+            Console.WriteLine($"Total amount pairs in 'counts2': {counts2.Count()}");
+            Console.WriteLine($"Total amount pairs in 'threads2': {threads2.Count()}");
+
+            Console.ReadLine();
+        }
+        async static Task Work(CultureInfo culture)
+        {
+            CultureInfo.CurrentUICulture = culture;
+            await Task.Delay(1000);
+            counts1.AddOrUpdate(CultureInfo.CurrentUICulture, 1, (_, ind) => ++ind);
+            threads1.AddOrUpdate(Thread.CurrentThread.ManagedThreadId, 1, (_, ind) => ++ind);
+            await AfterWork().ConfigureAwait(false);
+        }
+        async static Task AfterWork()
+        {
+            await Task.Delay(100);
+            counts2.AddOrUpdate(CultureInfo.CurrentUICulture, 1, (_, ind) => ++ind);
+            threads2.AddOrUpdate(Thread.CurrentThread.ManagedThreadId, 1, (_, ind) => ++ind);
+        }
+    }
+}
